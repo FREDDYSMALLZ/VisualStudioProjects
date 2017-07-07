9d08f38aa2b5c56f5b77ec55f2694db0ba20be34
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2017-07-07 01:39:20 -0500

    First commit

---
+++ IphoneCalculator/IphoneCalculator/IphoneCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IphoneCalculator
+{
+    public partial class IphoneCalculator : Form
+    {
+        public IphoneCalculator()
+        {
+            InitializeComponent();
+        }
+    }
+}
