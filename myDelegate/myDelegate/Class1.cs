f9c9124ceb537f05d27722a8fe4f9917a82828d1
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2017-07-08 14:17:28 -0500

    First Delegate demo project

---
+++ myDelegate/myDelegate/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace myDelegate
+{
+    public class Class1
+    {
+        delegate void delegateDemo(int i);
+        void Start()
+        {
+            delegateDemo Delegate = chocolate;
+            Delegate(45);
+        }
+
+        void chocolate(int i) //Method is compatible with the delegateDemo
+        {
+            Console.WriteLine(i);
+
+        }
+    }
+}
