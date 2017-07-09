78bfaa11742d5c4a3e98544191e9d72ff1d602f5
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2017-07-08 22:15:30 -0500

    Debugging Techniques, Without the debugger.

---
+++ Mistake1/Mistake1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Incoreect attempt to sum up the squares of
+ * numbers from 1 to a high value entered 
+ * by the user
+ * 
+ */
+namespace Mistake1
+{
+    public class Mistake1
+    {
+        //Use a loop to compute the sum of squares
+
+        static void Main(string[] args)
+        {
+            int sum = 0;   //Current value of the square and add
+            int count = 1;
+
+            Console.WriteLine("Enter the number of squares to sum:");
+
+            int high = int.Parse(Console.ReadLine());
+
+            while (count <= high) //No curly braces present
+                sum += count * count;
+            Console.WriteLine("The sum of the first {0} squares is {1}", high, sum);
+        }
+    }
+}
+/*Nothing happens to this program as well as it does not terminate. 
+ * Therefore, the user must terminate the program or abort the program.
+ * N/B- The WriteLine statement comes before the while loop
+ * and thus the WriteLine statement is never executed.
+*/ 
---
+++ Mistake1/Mistake3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Adds a WriteLine statement at the body
+ * of the while loop
+ * from the example given in Mistake1 program
+ * Modifys mistake 2 to increment the count
+*/
+namespace Mistake3
+{
+    public class Mistake3
+    {
+        //Use a loop to compute the sum of squares
+        static void Main(string[] args)
+        {
+
+            int sum = 0;   //Current value of the square and add
+            int count = 1;
+
+            Console.WriteLine("Enter the number of squares to sum:");
+
+            int high = int.Parse(Console.ReadLine());
+
+            while (count <= high) // Here the while loop body is made a block by use of curly braces
+            {
+                sum += count * count;
+                count++;
+               // Console.WriteLine("Sum is {0}", sum);
+            }
+            Console.WriteLine("The sum of the first {0} squares is {1}", high, sum);
+
+        }
+    }
+}
