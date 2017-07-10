a29fc0e424b98f0f4b4a5ed1fee35a3826ef2778
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2017-07-09 20:24:24 -0500

    Initial commit for the projects.

---
+++ Statements/Chapter3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Chapter3
+{
+   public  class Overtime
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the hours you worked this week:");
+            int hours = int.Parse(Console.ReadLine());
+
+            if (hours > 40)
+                Console.WriteLine("You worked overtime this week.");
+            Console.WriteLine("You worked {0} hours.", hours);
+
+        }
+    }
+}
