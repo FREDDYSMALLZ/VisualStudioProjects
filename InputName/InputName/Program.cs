3ed6b717ce7877d45256c1c854bd64361696123e
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2017-07-13 01:32:44 -0500

    Input name from the console initial program

---
+++ InputName/InputName/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InputName
+{
+   public  class InputName
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your name:");
+
+            String name = Console.ReadLine();
+
+            Console.WriteLine("Your name is {0}", name);
+        }
+    }
+}
