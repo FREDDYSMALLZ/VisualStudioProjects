691d923d1362b746418c17e1d94b40f7eef0f993
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2017-07-13 01:59:49 -0500

    Output format

---
+++ InputIneger/InputIneger/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace InputIneger
+{
+   public class InputIneger
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter an ineger number: ");
+            int number_1 = int.Parse(Console.ReadLine());
+
+            int number_2 = number_1 + number_1;
+
+            Console.WriteLine("Twice the number is {0}", number_2);
+            Console.WriteLine("The number is {0} and twice it is {1}", number_1, number_2);
+            Console.WriteLine("Twice the number is {0} and the number is {1}", number_2, number_1);
+        }
+    }
+}
