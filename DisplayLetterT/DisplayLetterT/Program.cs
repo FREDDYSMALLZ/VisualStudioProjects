c9577b87ec1a8ea998701b11b1d2e82108d96469
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2017-07-13 03:42:03 -0500

    Initial commit

---
+++ DisplayLetterT/DisplayLetterT/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+/* Write a program that displays a letter T
+ * like shown in the program design exercise
+ * 8 chapter 2
+*/
+
+namespace DisplayLetterT
+{
+  public  class DisplayLetterT
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine();
+        }
+    }
+}
