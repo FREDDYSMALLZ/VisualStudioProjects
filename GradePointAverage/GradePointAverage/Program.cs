9147b547d238fdc4c156782e0e241f39474f370c
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2017-07-15 04:49:22 -0500

    First commit of the program

---
+++ GradePointAverage/GradePointAverage/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Write C# program that checks a grade point
+ * average the user inputs and outputs
+ * congratulations, you made the honor roll if
+ * the average is 3.5 and above,
+ * but outputs Sorry, you are on probation if the 
+ * average is below 2.0
+*/
+
+using System;
+
+namespace GradePointAverage
+{
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
