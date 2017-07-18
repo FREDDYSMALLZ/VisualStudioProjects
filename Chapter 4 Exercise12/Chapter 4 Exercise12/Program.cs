f50d9dd1b6a5178092df8288d94c314880eee4db
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2017-07-18 05:14:30 -0500

    Initial commit

---
+++ Chapter 4 Exercise12/Chapter 4 Exercise12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Chapter_4_Exercise12
+{
+   public class Score
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Please enter the test score: ");
+            String input = Console.ReadLine();
+            int score = int.Parse(input);
+
+            switch (score )
+            {
+              
+
+            }
+        }
+    }
+}
