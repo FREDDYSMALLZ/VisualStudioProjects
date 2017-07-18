decb5b46a351100f092c46d3a87ba2b9605420f9
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2017-07-18 02:50:38 -0500

    Initial commit

---
+++ Example 3.6/Example 3.6/Program.cs
@@ -0,0 +1,41 @@
+﻿/*The program uses a while loop to compute
+ * the sum of test scores
+*/
+
+using System;
+
+namespace Example_3._6
+{
+   public class ScoreSum
+    {
+        //Returns the score entered by the user
+        //Use a constructor class
+        public static int Get_Score(String prompt)
+        {
+            Console.Write(prompt);
+            return int.Parse(Console.ReadLine());
+        }
+        public static int UpdateTotal (int newValue, int subTotal)
+        {
+            subTotal += newValue;
+            return subTotal;
+        }
+
+        public static void Main(string[] args)
+        {
+            int total = 0; //sum of the scores
+
+            int score = Get_Score("Enter the first score, -1 to quit: ");
+
+            while (score >= 0)
+            {
+                total = UpdateTotal(score, total);
+                score = Get_Score("Enter the next score, -1 to quit:");
+
+            }
+            Console.WriteLine("The total is {0}", total);
+
+            Console.ReadLine();
+        }
+    }
+}
