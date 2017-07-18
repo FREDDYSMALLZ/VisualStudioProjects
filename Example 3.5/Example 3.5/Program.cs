979fd0ac8577fe19ce797bee58c35e515b99ebcd
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2017-07-18 02:16:26 -0500

    Initial commit

---
+++ Example 3.5/Example 3.5/Program.cs
@@ -0,0 +1,40 @@
+﻿/*To convert degrees Centigrade to
+ * degrees Fhrenheit, the following
+ * formula is used
+ * F = 9C/5 + 32;
+*/
+
+using System;
+
+namespace Example_3._5
+{
+  public  class Temperature
+    {
+       public static void Main(string[] args)
+        {
+            double hotC;
+            double coldC;
+            double hotF;
+            double coldF;
+
+            Console.Write("Please Enter a hot temperature in celcius: ");
+            String input = Console.ReadLine();
+            hotC = double.Parse(input);
+            hotF = 9.0 * (hotC / 5.0) + 32.0;//From the formula given in the text
+
+            Console.WriteLine("The Fahrenheiht temperature is {0:F1}", hotF);
+
+            Console.Write("Please Enter a cold temperature in celcius: ");
+            input = Console.ReadLine();
+            coldC = double.Parse(input);
+            coldF = 9.0 * (coldC / 5.0) + 32.0; // From the above given formula
+
+            Console.WriteLine("The Fahrenheiht temperature is {0:F1}", coldF);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ Example 4.3/Example 4.3/Program.cs
@@ -0,0 +1,69 @@
+﻿/* The program is a modification of example
+ * 4.3 in the textbook which uses a swtch ststements
+ * instead of th nested if-else ststements
+*/
+
+using System;
+
+namespace Example_4._3
+{
+    public class Score
+    {
+       public static void Main(string[] args)
+        {
+            Console.Write("Enter a test score or -1 to quit: ");
+            int score = int.Parse(Console.ReadLine());
+            //int mark = score / 10;
+            switch (score)
+            {
+                case 0:
+                    (score <= 50).ToString();
+                    Console.WriteLine("F");
+                    break;
+                case 1:
+                    Console.WriteLine("F");
+                    break;
+                case 2:
+                    Console.WriteLine("F");
+                    break;
+                case 3:
+                    Console.WriteLine("F");
+                    break;
+                case 4:
+                    Console.WriteLine("F");
+                    break;
+                case 5:
+                    Console.WriteLine("F");
+                    break;
+                case 6:
+                    Console.WriteLine("F");
+                    break;
+                case 7:
+                    Console.WriteLine("F");
+                    break;
+                case 8:
+                    Console.WriteLine("F");
+                    break;
+                case 9:
+                    Console.WriteLine("F");
+                    break;
+                case10:
+                    Console.WriteLine("F");
+                    break;
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+            Console.WriteLine(" ");
+        }
+    }
+}
