44982172d4780465f139800e5c6268f92d87d6a6
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2017-07-18 04:49:25 -0500

    First commit

---
+++ Example 4.1/Example 4.1/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Evaluating AND, OR, and NOT expressions
+ * involving height and weight measurements.
+*/
+
+using System;
+
+namespace Example_4._1
+{
+   public class Measure
+    {
+       public static void Main(string[] args)
+        {
+            double height;       
+            Console.Write("Please enter the height in inches : ");           
+            String intput = Console.ReadLine();
+            height = double.Parse(intput);
+
+            double weight;
+            Console.Write("Please enter the weight in pounds: ");
+            weight = double.Parse(intput);
+
+            Console.Write("height < 65  && (weight < 130  is ");
+            Console.WriteLine((height <65) && (weight < 130 ));
+            Console.Write("height < 65  || (weight < 130  is ");
+            Console.WriteLine((height < 65) || (weight < 130));
+            bool heavy = (weight > 250);
+            Console.WriteLine("heavy is " + heavy);
+            Console.WriteLine("!heavy is " + !heavy);
+
+
+            Console.ReadLine();
+        }
+    }
+}
