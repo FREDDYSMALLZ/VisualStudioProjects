19178bff4133016c176719890447cf83c7f76510
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2017-07-18 15:40:56 -0500

    First commit

--- Chapter 4 Exercise12/Chapter 4 Exercise12/Program.cs
+++ Chapter 4 Exercise12/Chapter 4 Exercise12/Program.cs
@@ -9,10 +9,26 @@ namespace Chapter_4_Exercise12
             Console.Write("Please enter the test score: ");
             String input = Console.ReadLine();
             int score = int.Parse(input);
+            int mark = score / 10;
+            if (score >= 50)
+        
 
-            switch (score )
+            switch (mark )
             {
-              
+                case 0:                 
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                case 10:
+                        break;
+
+
 
             }
         }
---
+++ Example 4.10/Example 4.10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Example_4._10
+{
+    public class ConvertMenu
+    {
+        public static object IO { get; private set; }
+
+        public static void Metric_To_English()
+        {
+            Console.WriteLine("Converting from meters to yards, feet and inches: ");
+        }
+        public static void English_T0_Metric()
+        {
+            Console.WriteLine("Converting from yards, feet and inches to meters: ");
+
+        }
+        public static void Main(string[] args)
+        {
+            Console.Write("Please enter your choice");
+            String input = Console.ReadLine();
+            int choice = int.Parse(input);
+
+        }
+
+    }
+}
