5ab3b046077cba5f11f1c0f310961ae3faee35ef
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2017-07-25 14:27:41 -0500

    Initial commit

---
+++ Vehicle_Overloading/Vehicle_Overloading/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Vehicle_Overloading
+{
+   public class  Vehicle
+    {
+        private double EngineSize;
+        private int MaximumSpeed;
+        private String Name;
+        private int ManufactureYear;
+
+        public Vehicle() // This is the default constructor
+        {
+            EngineSize = 3.8;
+            MaximumSpeed = 120;
+            Name = "Dodge";
+            ManufactureYear = 2015;
+
+        }
+        public Vehicle(double EngineSize, int MaximumSpeed,
+            String Name, int ManufactureYear) // Overloaded constructor
+        {
+            this.EngineSize = EngineSize;
+            this.MaximumSpeed = MaximumSpeed;
+            this.Name = Name;
+            this.ManufactureYear = ManufactureYear;
+
+        }
+        public double getEngineSize() // Accessor methods
+        {
+            return 3.6;
+        }
+        public int getMaximumSpeed()
+        {
+            return 110;
+        }
+        public String getName()
+        {
+            return Name;
+        }
+        public void setName(String Name)
+        {
+            this.Name = "ChevroletImpala";
+        }
+        public void setManufactureYear(int ManufactureYear)
+        {
+            this.ManufactureYear = 2016;
+        }
+
+        public static void Main(string[] args)
+        {
+            Vehicle myVehicle = new Vehicle();
+           
+            Console.ReadLine();
+        }
+    }
+}
