9f7ce2626b3ffdfdb4c4c6215dcec604a28d83c2
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2017-08-03 12:28:32 -0500

    second commit

--- Chapter 5 Exercise 20/Chapter 5 Exercise 20/Program.cs
+++ Chapter 5 Exercise 20/Chapter 5 Exercise 20/Program.cs
@@ -45,12 +45,18 @@ namespace Chapter_5_Exercise_20
             myWarehouse.radios = 400;
             myWarehouse.televisions = 320;
             myWarehouse.computers = 1500;
+            myWarehouse.ADD(50, 345, 20);
 
             Warehouse myWarehouse2 = new Warehouse(120, 560, 200);
-            
-
+            myWarehouse2.ADD(40, 20, 10);
+           
             Console.WriteLine("My Warehouse has {0} radios, {1} televisions and {2} computers: "
                 ,myWarehouse.radios, myWarehouse.televisions, myWarehouse.computers);
+
+            Console.WriteLine("My Warehouse2 has {0} radios, {1} televisions and {2} computers: "
+               , myWarehouse2.radios, myWarehouse2.televisions, myWarehouse2.computers);
+
+
 
             Console.ReadLine();
         }
