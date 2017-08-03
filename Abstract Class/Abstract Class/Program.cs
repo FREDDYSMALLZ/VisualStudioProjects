b6a0b15572afe428608bc2a6d332e72cb3145f14
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2017-08-03 12:10:19 -0500

    Initial commit

---
+++ Abstract Class/Abstract Class/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Abstract_Class
+{
+   public abstract class GeometricShape
+    {
+        protected double radius;
+
+        public GeometricShape(double radius)
+        {
+            radius = radius; 
+        }
+            public abstract double getArea();
+            public abstract double getCircumference();
+    }
+    public class Circle: GeometricShape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = 4.77;
+        }
+        public double getArea()
+        {
+            return Math.PI * Math.Pow(radius, 2.0);
+        }
+        public double getCircumference()
+        {
+            return Math.PI * 2 * radius;
+        }
+    }
+        
+       public  static void Main(string[] args)
+        {
+            
+        }
+    }
---
+++ Chapter 5 Exercise 20/Chapter 5 Exercise 20/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Chapter_5_Exercise_20
+{
+   public class Warehouse
+    {
+        private int radios;
+        private int televisions;
+        private int computers;
+
+        public Warehouse()
+        {
+            radios = 0;
+            televisions = 0;
+            computers = 0;
+        }
+        public Warehouse (int Radios, int Televisions, int Computers)
+        {
+            radios = Radios;
+            televisions = Televisions;
+            computers = Computers;
+        }
+        public int  getRadios()
+        {
+            return radios;
+        }
+        public int getTelevisions()
+        {
+            return televisions;
+        }
+        public int getComputers()
+        {
+            return computers;
+        }
+        public void ADD(int Radios, int Televisions, int Computers)
+        {
+            radios += Radios;
+            televisions += Televisions;
+            computers += Computers;
+        }
+        
+        public  static void Main(string[] args)
+        {
+            Warehouse myWarehouse = new Warehouse();
+            myWarehouse.radios = 400;
+            myWarehouse.televisions = 320;
+            myWarehouse.computers = 1500;
+
+            Warehouse myWarehouse2 = new Warehouse(120, 560, 200);
+            
+
+            Console.WriteLine("My Warehouse has {0} radios, {1} televisions and {2} computers: "
+                ,myWarehouse.radios, myWarehouse.televisions, myWarehouse.computers);
+
+            Console.ReadLine();
+        }
+    }
+}
