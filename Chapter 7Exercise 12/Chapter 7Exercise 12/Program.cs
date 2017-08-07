981ad055f6c6b6eb106139f8b26c576c727a64c4
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2017-08-06 19:53:41 -0500

    Initial commit

---
+++ Chapter 7Exercise 12/Chapter 7Exercise 12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_7Exercise_12
+{
+   public class RandomNumbers
+    {
+        public static void Main(string[] args)
+        {
+            
+            
+                int[] Array = new int[10000];
+                Array[0] = 1;
+                Array[1] = 2;
+                Array[2] = 3;
+                Array[3] = 4;
+                Array[4] = 5;
+
+                Random myRandom = new Random();
+                int number = myRandom.Next(Array.Length);
+
+                Console.WriteLine(Array[number]);
+
+
+                Console.ReadLine();
+            }
+    }
+}
