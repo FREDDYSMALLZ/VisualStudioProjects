b31c1883dbff1177a3b41263c17ad394416b87b1
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2017-08-15 03:46:44 -0500

    Initial Commit

---
+++ Chapter 6 Exercise 18/Chapter 6 Exercise 18/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_6_Exercise_18
+{
+   partial class IBendable
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("");
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
