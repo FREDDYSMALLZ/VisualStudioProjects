52806a713241e6a372afe5f427f88e38c9315d81
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2017-08-15 03:33:27 -0500

    Commit

---
+++ Chapter 10 Exercise18/Chapter 10 Exercise18/Circle_Line.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter_10_Exercise18
+{
+    public partial class Circle_Line : Form
+    {
+        public Circle_Line()
+        {
+            InitializeComponent();
+        }
+    }
+}
