d7eb405a59641a8d0429c85a20afee6013dc9c05
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2017-08-16 03:00:05 -0500

    Commit

--- Chapter 6 Exercise 18/Chapter 6 Exercise 18/Program.cs
+++ Chapter 6 Exercise 18/Chapter 6 Exercise 18/Program.cs
@@ -6,18 +6,65 @@ using System.Threading.Tasks;
 
 namespace Chapter_6_Exercise_18
 {
-   partial class IBendable
+    public interface IBendable
     {
+        void Bend();
+
+    }
+    
+   public class Spoon: IBendable
+    {
+        public void Eat()
+        {
+            Console.WriteLine("The Spoon is used for eating.");
+
+        }
+        public class Arm : IBendable
+        {
+            public void Bend()
+            {
+                Console.WriteLine("The Arm can bend.");
+
+            }
+            public void Raise()
+            {
+
+            }
+        }
+        public class TestIBendable
+        {
+            public static void Bend(IBendable B)
+            {
+                B.Bend();
+                
+            }
+        }
         public static void Main(string[] args)
         {
+            Spoon newSpoon = new Spoon();
+            Arm newArm = new Arm();
+
+            Bend(newSpoon);
 
             Console.WriteLine("");
+            Bend(newArm);
+        }
 
+        private static void Bend(Arm newArm)
+        {
+            
+        }
 
+        private static void Bend(Spoon newSpoon)
+        {
+            
+        }
 
-
-
+        public void Bend()
+        {
             Console.ReadLine();
         }
+
     }
+
 }
