3f7a2ae99774d152ea4ebe80de1ca1eca40620ac
Fredrick Onduso Ondieki <fredieondieki@outlook.com>
2017-08-16 14:21:24 -0500

    Initial commit

---
+++ Chapter 3 Exerciese 22/Chapter 3 Exerciese 22/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_3_Exerciese_22
+{
+    public  class SimpleInterest
+    {
+        double Interest, interest_Rate, Number_Of_Years;
+        double Balance, Final_Balance;
+        public static void Main(string[] args)
+        {
+
+            Console.Write("Please Enter the Initial account balance: ");
+            String input = Console.ReadLine();
+            double Balance = double.Parse(input);
+
+            Console.Write("Please Enter the inerest rate: ");
+            String input1 = Console.ReadLine();
+            double interest_Rate = double.Parse(input1);
+
+            Console.Write("Please Enter the Final account balance: ");
+            String input2 = Console.ReadLine();
+            double Final_Balance = double.Parse(input2);
+
+            double Number_Of_Years = (1 / (interest_Rate / 100)) * ((Final_Balance / Balance) - 1);
+
+
+            Console.WriteLine("");
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }      
+    }
+}
